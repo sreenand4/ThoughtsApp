3d2073681f7545f4afbb0ea4d7683561b173e26b
sreenandm <148009692+sreenandm@users.noreply.github.com>
2024-07-12 11:10:47 -0400

    .

--- backend/controllers/commentsController.js
+++ backend/controllers/commentsController.js
@@ -1,23 +1,29 @@
 const Comment = require('../models/CommentModel');
-const mongoose = require('mongoose')
 
 const commentOnThought = async (req, res) => {
-    const { thoughtId } = req.params;
-
-    // const { authorId, authorUsername, content } = req.body;
+    const { authorId, authorUsername, content, parentThoughtId } = req.body;
     try {
-        res.status(200).json(thoughtId);
-        // const comment = await Comment.create({
-        //     authorId: authorId,
-        //     authorUsername: authorUsername,
-        //     parentThoughtId: thoughtId,
-        //     content: content,
-        //     likeCount: 0,
-        // });
-        // res.status(200).json(comment);
+        const comment = await Comment.create({
+            parentThoughtId: parentThoughtId,
+            authorId: authorId,
+            authorUsername: authorUsername,
+            content: content,
+            likeCount: 0,
+        });
+        res.status(200).json(comment);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 }
 
-module.exports = commentOnThought;
+const getCommentsByParentThoughtId = async (req, res) => {
+    const { parentThoughtId } = req.params;
+    try {
+        const comments = await Comment.find({ parentThoughtId: parentThoughtId });
+        res.status(200).json(comments);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = { commentOnThought, getCommentsByParentThoughtId };
--- backend/endpoints/comments.js
+++ backend/endpoints/comments.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const Comment = require('../models/CommentModel');
-const commentOnThought = require('../controllers/commentsController')
+const { commentOnThought, getCommentsByParentThoughtId } = require('../controllers/commentsController')
 
 const router = express.Router();
 
 // POST a thoguht 
-router.post('/commentOnThought/:thoughtId', (req, res) => {
-    const { thoughtId } = req.params;
-    console.log('Received thoughtId:', thoughtId);
-    try {
-        res.status(200).json({ thoughtId });
-    } catch (err) {
-        console.log(err.message)
-        res.status(400).json({ error: err.message })
-    }
-});
+router.post('/commentOnThought', commentOnThought);
+router.get('/commentsOnThought/:parentThoughtId', getCommentsByParentThoughtId);
 
 module.exports = router;
